d41b8a3b44ca0407396b0be86e062545b3b038dc
Willem Monsuwe <willem@monsuwe.nl>
2020-02-24 20:23:26 +0100

    Tweaks

--- index.js
+++ index.js
@@ -1,17 +1,11 @@
 var express = require('express')
-var http = require('http')
-var path = require('path')
-var socketio = require('socket.io')
 var app = express()
-var server = http.Server(app)
-var io = socketio(server)
-app.set('port',5000)
-app.use('/static', express.static(path.join(__dirname, 'static')))
-app.get('/', function(request, response) {
-    response.sendFile(path.join(__dirname, 'index.html'))
-})
-server.listen(5000, function() {
-    console.log('Starting server on port 5000')
+var http = require('http').createServer(app)
+var io = require('socket.io')(http)
+var path = require('path')
+app.use('/', express.static(path.join(__dirname, 'public')))
+http.listen(80, function() {
+    console.log('Starting server on port 80')
 })
 
 let basenames = [ 'Charlie','Alpha','Echo','Bravo','Delta' ]
